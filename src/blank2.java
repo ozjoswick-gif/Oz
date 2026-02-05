b1882cac0a7ea1183bfc75c192b5700b7db1a820
andrew.yaitsky <andrew.yaitsky@mtbethelchristian.org>
2026-02-04 23:08:11 -0500

    I AM FrEEEEEEE

---
+++ src/blank2.java
@@ -0,0 +1,41 @@
+import Developer.Pose;
+import Developer.Renderer;
+import Developer.Robot;
+
+public class blank2 {
+    private Robot decbot;
+    private Renderer renderer;
+    //------------------
+
+    //Literally anything
+
+
+
+    //------------------
+    public static void main(String[] args) {
+        new blank2().start();
+    }
+    private void start() {
+        decbot = new Robot();
+        renderer = new Renderer(decbot);
+        decbot.setPose(new Pose(10.0, 0.0, 0.0));
+        decbot.setInconsistency(0.05);
+        //------------------
+
+
+
+        //------------------
+        renderer.runOnEDT(() -> {
+            renderer.initWindow();
+            renderer.startLoop(this::loop, 20);
+        });
+    }
+    private void loop() {
+        double dt = 0.02;
+        //------------------
+
+        //------------------
+        decbot.update(dt);
+    }
+
+}
